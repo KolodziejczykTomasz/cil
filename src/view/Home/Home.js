ab120573b331e4d1b84bdfaaa6bf897af0dc9c44
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-11-04 09:56:09 +0100

    Removed aos from footer component

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -7,11 +7,10 @@ import Portfolio from '../../component/Portfolio/Portfolio';
 import NavBar from '../../component/NavBar/NavBar';
 import Latest from '../../component/Latest/Latest';
 import Logotyp from '../../component/Logotyp/Logotyp';
-import CookieConsent from "react-cookie-consent";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import CookieConsent from 'react-cookie-consent';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './Home.css';
-
 
 class Home extends Component {
   state = {
@@ -19,15 +18,15 @@ class Home extends Component {
     fontSizeChange: 14,
   };
 
-  componentDidMount(){
+  componentDidMount() {
     AOS.init({
-      duration: 1000
-    })
-  };
+      duration: 1000,
+    });
+  }
 
   handleResetFontSize = () => {
     this.setState({
-      fontSizeChange: 14,  
+      fontSizeChange: 14,
     });
   };
 
@@ -53,41 +52,55 @@ class Home extends Component {
 
   render() {
     const { fontSizeChange } = this.state;
-   
 
     return (
       <Fragment>
         <div id="wrapperHome">
-        <div
-          className={this.state.button ? 'buttonTrue' : 'buttonFalse'}
-          style={{ fontSize: fontSizeChange }}
-        >
-          <NavBar
-            clickColorFn={this.handleClick}
-            growFontFn={this.handleGrowFontSize}
-            shrinkFontFn={this.handleShrinkFontSize}
-            resetFontFn={this.handleResetFontSize}
-          />
-          <Slide />
-          <div data-aos="fade-up"><Latest clickColorFn={this.handleClick} /></div>
-          <div data-aos="fade-up"><Portfolio clickColorFn={this.handleClick} /></div>
-          <div data-aos="fade-up"><Logotyp clickColorFn={this.handleClick} /></div>
-          <div data-aos="fade-up"><Footer clickColorFn={this.handleClick} /></div>                            
-          
-          <ScrollUpButton />
+          <div
+            className={this.state.button ? 'buttonTrue' : 'buttonFalse'}
+            style={{ fontSize: fontSizeChange }}
+          >
+            <NavBar
+              clickColorFn={this.handleClick}
+              growFontFn={this.handleGrowFontSize}
+              shrinkFontFn={this.handleShrinkFontSize}
+              resetFontFn={this.handleResetFontSize}
+            />
+            <Slide />
+            <div data-aos="fade-up">
+              <Latest clickColorFn={this.handleClick} />
+            </div>
+            <div data-aos="fade-up">
+              <Portfolio clickColorFn={this.handleClick} />
+            </div>
+            <div data-aos="fade-up">
+              <Logotyp clickColorFn={this.handleClick} />
+            </div>
+            <div>
+              <Footer clickColorFn={this.handleClick} />
+            </div>
+
+            <ScrollUpButton />
+          </div>
+          <CookieConsent
+            location="bottom"
+            buttonText="Wyrażam zgodę"
+            cookieName="myAwesomeCookieName2"
+            style={{ background: '#2B373B', width: '100%' }}
+            buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+            expires={150}
+          >
+                 
+            <span id="cookieSpan">
+               Nasz serwis, jak większość serwisów internetowych, wykorzystuje tzw. pliki cookies.
+              Korzystanie z serwisu oznacza zgodę na ich zapis lub wykorzystanie. Więcej informacji
+              można znaleźć w „Polityce prywatności”. Akceptuję{' '}
+              <Link to="/cookies">"Politykę prywatności"</Link> i wykorzystania plików cookies w
+              serwisie.
+            </span>
+                
+          </CookieConsent>
         </div>
-        <CookieConsent
-      location="bottom"
-      buttonText="Wyrażam zgodę"
-      cookieName="myAwesomeCookieName2"
-      style={{ background: "#2B373B", width: '100%' }}
-      buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-      expires={150}
-    >
-     <span id="cookieSpan"> Nasz serwis, jak większość serwisów     internetowych, wykorzystuje tzw. pliki cookies. Korzystanie z serwisu oznacza zgodę na ich zapis lub wykorzystanie. Więcej informacji można znaleźć w „Polityce prywatności”. 
-Akceptuję <Link to="/cookies" >"Politykę prywatności"</Link> i wykorzystania plików cookies w serwisie.</span>
-    </CookieConsent>
-</div>
       </Fragment>
     );
   }
